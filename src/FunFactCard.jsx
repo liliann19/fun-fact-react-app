7414a8e6ffd7090036db93010e6021a91bd24c3d
Lilian <210286008+liliann19@users.noreply.github.com>
2026-04-09 10:48:16 -0700

    completed step 1 of creating FunFactCard file and careting component

---
+++ src/FunFactCard.jsx
@@ -0,0 +1,10 @@
+function FunFactCard() {
+    return (
+        <div>
+            <h2>Fun Fact</h2>
+            <p>This is a fun fact.</p>
+        </div>
+    );
+}
+
+export default FunFactCard;
