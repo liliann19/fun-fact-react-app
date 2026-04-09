e0943c8abac29f2f7048f8ea1274f75619b1e723
Lilian <210286008+liliann19@users.noreply.github.com>
2026-04-09 10:55:57 -0700

    completed step 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import FunFactCard from './FunFactCard'
 function App() {
   return (
     <div>
-      <FunFactCard fact="I have visited more than 10 countries." />
+      <FunFactCard fact="I have visited 3 states." />
+      <FunFactCard fact="I started programming in college." />
+      <FunFactCard fact="I like to solve Sudoku puzzles." />
+      <FunFactCard fact="I got my first computer in 2011." />
+      <FunFactCard fact="I have a dog named Lilith." />
     </div>
   )
 }
